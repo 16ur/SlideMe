0a755a902b9cb3ed0dc24b0afcb7a5c5055d403d
MANGUIAN Axel <axel.manguian@etu.univ-amu.fr>
2024-02-14 11:55:33 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import Slider from "./mycomponents/Slider";
 import "./App.css";
+import slidesData from "./slides.json";
 
 const App = () => {
-  const slides = [
-    {
-      titre: "1 Bonjour les amis",
-      contenu: "Premier test de slider avec React. ",
-      image: reactLogo,
-    },
-    {
-      titre: "2 Deuxième slide",
-      contenu:
-        "Deuxième slide avec un contenu différent. On peut mettre ce qu'on veut.",
-      image: viteLogo,
-    },
-    {
-      titre: "3 Lucas",
-      contenu: "Gros con de première",
-      image: reactLogo,
-    },
-  ];
   const [index, setIndex] = useState(0);
-  const slide = slides[index];
-  const total = slides.length;
+  const slide = slidesData[index];
+  const total = slidesData.length;
 
   const handlePrev = () => {
     setIndex((index - 1 + total) % total);
@@ -36,18 +18,15 @@ const App = () => {
   };
 
   return (
-    <div class="">
-      <div class="h-96 bg-stone-50 shadow-md rounded-md ml-10 mr-10 mt-10 border-solid border-0 border-black">
+    <div className="">
+      <div className="h-[33rem] bg-stone-50 shadow-md rounded-md ml-10 mr-10 mt-10 border-solid border-0 border-black">
         <Slider
-          titre={<h1 style={{ fontSize: "27px" }}>{slide.titre}</h1>}
+          titre={<div style={{ fontSize: "29px" }}>{slide.titre}</div>}
           contenu={slide.contenu}
           image={slide.image}
         />
       </div>
-      <div class="flex justify-center">
-        <p>
-          Slide {index + 1} of {total}
-        </p>{" "}
+      <div className="flex justify-center">
         <button
           onClick={handlePrev}
           className="mx-2 mt-10 bg-zinc-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
@@ -60,6 +39,13 @@ const App = () => {
         >
           Suivant
         </button>
+      </div>
+      <div className="flex justify-center">
+        <div>
+          <p>
+            Slide {index + 1} of {total}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/mycomponents/Slider.jsx
+++ src/mycomponents/Slider.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 export default Slider;
 
-function Slider({ titre, contenu }) {
+function Slider({ titre, contenu, image }) {
   return (
     <div>
       <h1>{titre}</h1>
       <p>{contenu}</p>
+      <img src={image} alt="image" />
     </div>
   );
 }
